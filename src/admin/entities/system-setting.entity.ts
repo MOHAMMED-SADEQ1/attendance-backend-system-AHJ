9a580ffca3516e4be4fc033a7210c2f33327100d
MOHAMMED-SADEQ1 <147517922+MOHAMMED-SADEQ1@users.noreply.github.com>
2026-06-11 17:41:40 +0300

    update

--- src/admin/entities/system-setting.entity.ts
+++ src/admin/entities/system-setting.entity.ts
@@ -7,7 +7,7 @@ import {
 
 @Entity('system_settings')
 export class SystemSetting {
-  @PrimaryGeneratedColumn({ type: 'tinyint', unsigned: true })
+  @PrimaryGeneratedColumn({ type: 'smallint' })
   id: number;
 
   @Column({ name: 'setting_key', length: 100, unique: true })
@@ -22,3 +22,30 @@ export class SystemSetting {
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
+
+
+
+// import {
+//   Entity,
+//   PrimaryGeneratedColumn,
+//   Column,
+//   UpdateDateColumn,
+// } from 'typeorm';
+
+// @Entity('system_settings')
+// export class SystemSetting {
+//   @PrimaryGeneratedColumn({ type: 'tinyint', unsigned: true })
+//   id: number;
+
+//   @Column({ name: 'setting_key', length: 100, unique: true })
+//   settingKey: string;
+
+//   @Column({ name: 'setting_value', type: 'text', nullable: true })
+//   settingValue: string;
+
+//   @Column({ type: 'text', nullable: true })
+//   description: string;
+
+//   @UpdateDateColumn({ name: 'updated_at' })
+//   updatedAt: Date;
+// }
--- src/app.module.ts
+++ src/app.module.ts
@@ -13,37 +13,23 @@ import { AttendanceModule } from './attendance/attendance.module';
   imports: [
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
-
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'u450546278_attend',
-      password: ':bQqao5u3K',
-      database: 'u450546278_attendance_db',
-
-
-      // type: 'mysql',
-      // host: 'localhost',
-      // port: 3306,
-      // username: 'root',
-      // password: '124536',
-      // database: 'attendance_db',
-
-
-      // type: 'postgres',
-      // host: 'dpg-d8i1u2lckfvc73bbfcc0-a',
-      // port: 5432,
-      // username: 'attendance_db_4bpr_user',
-      // password: '8b61qhDpm7B0y1l7aNAxZ2BwZFHz68xv',
-      // database: 'attendance_db_4bpr',
-
-
+      type: 'postgres',
+      host: 'dpg-d8i1u2lckfvc73bbfcc0-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'attendance_db_4bpr_user',
+      password: '8b61qhDpm7B0y1l7aNAxZ2BwZFHz68xv',
+      database: 'attendance_db_4bpr',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      extra: {
+        timezone: '+03:00', // المنطقة الزمنية لجلسة PostgreSQL
+      },
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
       migrationsRun: false,
       synchronize: false,
       logging: false,
-      timezone: '+03:00',
     }),
     AuthModule,
     EmployeesModule,
@@ -65,3 +51,79 @@ import { AttendanceModule } from './attendance/attendance.module';
   ],
 })
 export class AppModule {}
+
+
+
+
+
+
+
+
+
+// import { Module, ValidationPipe } from '@nestjs/common';
+// import { APP_PIPE } from '@nestjs/core';
+// import { ScheduleModule } from '@nestjs/schedule';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AuthModule } from './auth/auth.module';
+// import { EmployeesModule } from './employees/employees.module';
+// import { AdminModule } from './admin/admin.module';
+// import { AttendanceModule } from './attendance/attendance.module';
+
+// @Module({
+//   imports: [
+//     ScheduleModule.forRoot(),
+//     TypeOrmModule.forRoot({
+
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'u450546278_attend',
+//       password: ':bQqao5u3K',
+//       database: 'u450546278_attendance_db',
+
+
+//       // type: 'mysql',
+//       // host: 'localhost',
+//       // port: 3306,
+//       // username: 'root',
+//       // password: '124536',
+//       // database: 'attendance_db',
+
+
+//       // type: 'postgres',
+//       // host: 'dpg-d8i1u2lckfvc73bbfcc0-a',
+//       // port: 5432,
+//       // username: 'attendance_db_4bpr_user',
+//       // password: '8b61qhDpm7B0y1l7aNAxZ2BwZFHz68xv',
+//       // database: 'attendance_db_4bpr',
+
+
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+//       migrationsRun: false,
+//       synchronize: false,
+//       logging: false,
+//       timezone: '+03:00',
+//     }),
+//     AuthModule,
+//     EmployeesModule,
+//     AdminModule,
+//     AttendanceModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [
+//     AppService,
+//     {
+//       provide: APP_PIPE,
+//       useFactory: () =>
+//         new ValidationPipe({
+//           whitelist: true,
+//           forbidNonWhitelisted: true,
+//           transform: true,
+//         }),
+//     },
+//   ],
+// })
+// export class AppModule {}
